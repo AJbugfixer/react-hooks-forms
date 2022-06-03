0206041fd932fdeb8c08c34ec521b948396233b4
Ajay <ajay.ip3105@gmail.com>
2022-06-03 00:02:16 -0400

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form ({handelFirstName, handelLastName, firstName, lastName}){
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handelFirstName} value={firstName} />
+      <input type="text" onChange={handelLastName} value={lastName} />
       <button type="submit">Submit</button>
     </form>
-  );
+  )
 }
 
-export default Form;
+export default Form
